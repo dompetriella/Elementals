72f1f5e273c6b3014b3bc1da8bcc46d559686ba7
Dominic Petriella <dpetriella@magicwrighter.com>
2023-01-20 14:30:38 -0500

    made basic game page layout

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -9,19 +9,190 @@ class GamePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.lightBlue,
-        body: Container(
-          color: Colors.lightBlue,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                enableFeedback: false,
+        body: Column(
+          children: [
+            Flexible(
+              flex: 5,
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      OpponentSide(),
+                      PlayerSide(),
+                    ],
+                  ),
+                  SettingsButton(),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 5)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 75,
+                          width: 50,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              onPressed: () => context.go('/'),
-              child: Text("Go back"),
+            ),
+            Flexible(flex: 3, child: PlayerHandArea())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsButton extends StatelessWidget {
+  const SettingsButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      bottom: 0,
+      left: 0,
+      child: Center(
+        child: GestureDetector(
+          onTap: () => context.go('/'),
+          child: Container(
+            height: 80,
+            width: 40,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.horizontal(
+                    left: Radius.zero, right: Radius.circular(10)),
+                boxShadow: [BoxShadow(color: Colors.white, spreadRadius: 2)],
+                color: Colors.blue),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                  child: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              )),
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class OpponentSide extends StatelessWidget {
+  const OpponentSide({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 20,
+      child: Container(
+          decoration: BoxDecoration(
+        color: Colors.orange,
+      )),
+    );
+  }
+}
+
+class PlayerSide extends StatelessWidget {
+  const PlayerSide({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 20,
+      child: Container(
+          decoration: BoxDecoration(
+        color: Colors.lightGreen,
+        border: Border(top: BorderSide(color: Colors.white, width: 5)),
+      )),
+    );
+  }
+}
+
+class PlayerHandArea extends StatelessWidget {
+  const PlayerHandArea({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.green,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Container(
+                  color: Colors.green.shade300,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 6.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        DummyCard(),
+                        DummyCard(),
+                        DummyCard(),
+                        DummyCard(),
+                        DummyCard(),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 80,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green.shade800,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.white, width: 2)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'End Turn',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+    );
+  }
+}
+
+class DummyCard extends StatelessWidget {
+  const DummyCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 2.0),
+      child: Container(
+        height: 100,
+        width: 60,
+        color: Colors.black,
       ),
     );
   }
