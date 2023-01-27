e20a1d404df806cada4a866230caa9fa67e02d8b
Dominic Petriella <dpetriella@magicwrighter.com>
2023-01-27 15:35:48 -0500

    begin startup code

--- lib/game_logic/startup.dart
+++ lib/game_logic/startup.dart
@@ -1,11 +1,17 @@
+import 'dart:math';
+
 import 'package:elementals/providers/playerProvider.dart';
 import 'package:flutter_guid/flutter_guid.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/enums.dart';
 
 setInitalPlayerProperties(WidgetRef ref) {
   var playerOneState = ref.watch(playerProvider.notifier).state;
   var playerTwoState = ref.watch(opponentProvider.notifier).state;
 
   playerOneState = playerOneState.copyWith(id: Guid.generate().toString());
-  playerTwoState = playerTwoState.copyWith(id: Guid.generate().toString());
+  playerTwoState = playerTwoState.copyWith(
+      id: Guid.generate().toString(),
+      elementalType: ElementalType.values[Random().nextInt(3)]);
 }
