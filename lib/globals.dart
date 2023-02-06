8863fb6da60f85613f965f90983720cdc7d00f8d
dompetriella <dompetriella@gmail.com>
2023-02-06 08:55:28 -0500

    added firebase host

--- lib/globals.dart
+++ lib/globals.dart
@@ -8,16 +8,6 @@ const int highestCardValue = 7;
 
 const int winningScore = 30;
 
-const double cardHeightP1 = 90;
-
-const double cardWidthProportion = 0.58;
-
-const double cardHeightP2 = cardHeightP1 * 0.70;
-
-const double playerIconDisplaySize = 60;
-
-const double cardSpacing = 5;
-
 const int fireBonus = 5;
 const int fireMax = 10;
 const int firePenalty = -1;
@@ -33,3 +23,13 @@ const int earthBonus = 7;
 // ui
 int gameField = 5;
 int playerField = 3;
+
+const double cardHeightP1 = 90;
+
+const double cardWidthProportion = 0.58;
+
+const double cardHeightP2 = cardHeightP1 * 0.70;
+
+const double playerIconDisplaySize = 60;
+
+const double cardSpacing = 5;
